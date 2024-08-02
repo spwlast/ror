b96343c54bde466f3771798e39a9c6841fa8759e
spwlast <akbarfawwaz271@gmail.com>
2024-08-02 23:20:27 +0900

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
+global.owner = ['6283866274502']
+global.ownernomer = "623866274502"
+global.premium = ['6283866274502']
 global.packname = 'Sticker By'
-global.author = 'Naze Dev'
+global.author = 'pawas Dev'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.jumlha = '999'
 global.jumhal = '100000000000000'
